138f94b481df5260af8ee38549d8fcb89f42c894
Jazzyra <127509175+JihadZaidan@users.noreply.github.com>
2025-07-30 13:02:56 +0700

    feat: added section brand feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import VisionBrand from "@/components/landing-page/brands";
 import Introduce from "@/components/landing-page/introduce";
 
 export default function RootPage() {
   return (
     <div className="w-full">
       <Introduce/>
+      <VisionBrand/>
     </div>
   );
 }
