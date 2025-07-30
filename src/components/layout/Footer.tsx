6df09c6b0b838236126880f4886503d72a7ffef2
Jazzyra <127509175+JihadZaidan@users.noreply.github.com>
2025-07-30 10:26:58 +0700

    feat(footer): styling responsive position

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,14 +6,14 @@ export default function Footer() {
     return (
         <div className="max-w-full w-full lg:px-20 lg:py-20 sm:px-5 sm:pt-14 sm:pb-6 flex flex-col gap-30 bg-black">
             <div className="w-full gap-18 flex lg:flex-row sm:flex-col">
-                <h1 className="w-[70%] leading-[130%] text-7xl text-white font-sans font-normal">
+                <h1 className="lg:w-[70%] w-[90%] leading-[130%] text-7xl text-white lg:text-left  font-sans font-normal">
                     Let&apos;s Make Something Cool
                 </h1>
-                <ul className="w-full flex flex-col gap-4 justify-right items-right">
+                <ul className="w-full flex flex-col gap-7 justify-right items-right">
                     {navigation
                         .filter((item) => item.hideInNavbar || item.published)
                         .map((item) => (
-                            <li key={item.url} className="nav-item font-medium text-right text-white text-xl">
+                            <li key={item.url} className="nav-item font-medium lg:text-right lg:ml-0 ml-2 text-left text-white text-2xl">
                                 <Link href={item.url} className="hover:text-white">
                                     {item.label}
                                 </Link>
@@ -21,7 +21,7 @@ export default function Footer() {
                         ))}
                 </ul>
             </div>
-            <div className="border-t border-transparent flex flex-wrap gap-4 justify-between items-center text-white">
+            <div className="border-t border-transparent flex lg:flex-row flex-col gap-4 justify-between items-center text-white">
                 <p className="text-center md:text-left">Copyright © 2024 Nexa</p>
                 <div className="flex items-center cursor-pointer gap-2">
                     <Link href="#" className="hover:text-white">
