fcc14aa81b4b419007967e1f5c6d299f78fd5b94
Jazzyra <127509175+JihadZaidan@users.noreply.github.com>
2025-07-31 14:04:03 +0700

    style & feat(page): added work section and response setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import VisionBrand from "@/components/landing-page/brands";
 import Introduce from "@/components/landing-page/introduce";
+import Worksheet from "@/components/landing-page/projects";
 
 export default function RootPage() {
   return (
     <div className="w-full">
       <Introduce/>
       <VisionBrand/>
+      <Worksheet/>
     </div>
   );
 }
