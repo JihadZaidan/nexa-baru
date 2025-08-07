13d04c781d3f7d9cd337a30f0b26e5310bc642e8
Jazzyra <127509175+JihadZaidan@users.noreply.github.com>
2025-08-07 12:31:16 +0700

    Add files via upload

---
+++ src/components/insight/insightHead.tsx
@@ -0,0 +1,19 @@
+export default function Insider() {
+    return (
+        <div className="max-w-full w-full bg-white lg:px-20 lg:pt-16 lg:pb-20 px-9 pt-9 pb-14">
+            <div className="w-full justify-left items-left flex flex-col lg:gap-25 gap-16">
+                <h1 className="w-full text-left text-5xl font-medium font-sans text-black leading-[150%]">
+                    Digital Diaries: <br />
+                    Strategies and Insights for Success
+                </h1>
+
+                <p className="lg:text-right text-black font-sans font-medium text-3xl leading-[140%] lg:ml-50">
+                    — Unlocking Strategies, Trends, <br/>
+                    and Insights to Propel Your Brand <br/>
+                    Forward in the Dynamic World of <br/>
+                    Digital Marketing.
+                </p>
+            </div>
+        </div>
+    )
+}
