b8d79ef19de4aad204add19b6d2cbade3c7c05d0
Jazzyra <127509175+JihadZaidan@users.noreply.github.com>
2025-08-07 15:45:09 +0700

    feat(blog): insight page import and added data blog

--- src/app/insight/page.tsx
+++ src/app/insight/page.tsx
@@ -1,9 +1,12 @@
 import Insider from "@/components/insight/insightHead";
+import InsightSection from "@/components/insight/blog";
 
 export default function Insight() {
     return (
         <div className="w-full">
             <Insider/>
+            <InsightSection/>
         </div>
     )
+
 }
