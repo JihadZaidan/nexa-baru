698ea225b1bad037e0ced322f46fa1d1c6fcf921
Jazzyra <127509175+JihadZaidan@users.noreply.github.com>
2025-08-07 12:35:22 +0700

    Add files via upload

---
+++ src/app/insight/page.tsx
@@ -0,0 +1,9 @@
+import Insider from "@/components/insight/insightHead";
+
+export default function Insight() {
+    return (
+        <div className="w-full">
+            <Insider/>
+        </div>
+    )
+}
