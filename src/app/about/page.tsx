2aac838f2f7644bf9b07599682975f008f819248
Jazzyra <127509175+JihadZaidan@users.noreply.github.com>
2025-08-08 10:14:08 +0700

    feat(about): added teams view

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,15 @@
 import UpperAbout from "@/components/about/aboutHead";
 import AboutExplor from "@/components/about/explorer";
+import Teams from "@/components/about/teams";
 
 export default function About() {
     return (
         <div className="w-full">
             <UpperAbout/>
             <AboutExplor/>
+            <Teams/>
         </div>
     )
 
 }
+
