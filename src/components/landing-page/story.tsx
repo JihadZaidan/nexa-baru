f09c5d32ddd986445421400c03d85b4a5b2abe81
Jazzyra <127509175+JihadZaidan@users.noreply.github.com>
2025-08-08 15:30:30 +0700

    feat(landing): improve gsap effect

--- src/components/landing-page/story.tsx
+++ src/components/landing-page/story.tsx
@@ -1,20 +1,33 @@
+"use client"
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { ArrowRight } from "lucide-react";
 import invidi from "../../../public/landing-photos/insight/insight-visa.png"
 import invida from "../../../public/landing-photos/insight/insight-vida.png"
+import { useRef } from "react";
+import { useSlideFromLeft, useSlideFromRight, useZoom } from "@/lib/animations"
 
 export default function Story() {
+    const headRef = useRef<HTMLHeadingElement>(null);
+    const buttonRef = useRef<HTMLDivElement>(null);
+    const oneRef = useRef<HTMLDivElement>(null);
+    const twoRef = useRef<HTMLDivElement>(null);
+
+    useSlideFromLeft(headRef, 0.3);
+    useSlideFromRight(buttonRef, 0.3);
+    useZoom(oneRef, 0.3);
+    useZoom(twoRef, 0.3);
+
     return (
         <div className="max-w-full w-full bg-[#F2F2F2] px-20 py-30">
             <div className="w-full flex flex-col justify-left items-left gap-30">
                 <div className="w-full flex lg:flex-row flex-col lg:justify-between justify-left items-left gap-10">
-                    <h3 className="lg:w-[48%] w-full text-left text-5xl font-medium font-sans text-black leading-[150%]">
+                    <h3 ref={headRef} className="lg:w-[48%] w-full text-left text-5xl font-medium font-sans text-black leading-[150%]">
                         Latest insights, stories, <br />
                         and news from Nexa.
                     </h3>
 
-                    <div className="relative lg:mt-25">
+                    <div ref={buttonRef} className="relative lg:mt-25">
                         <Button className="flex flex-row justify-center gap-3 ml-[-20px]">
                             <p className="text-black lg:text-lg text-2xl font-medium">Explore the Insight</p>
                             <ArrowRight height={22} width={22} className="mt-[2px] text-black lg:h-[22px] lg:w-[22px] h-[25px] w-[25px]" />
@@ -23,7 +36,7 @@ export default function Story() {
                 </div>
 
                 <div className="w-full flex lg:flex-row flex-col lg:gap-20 gap-10 justify-left items-left">
-                    <div className="w-full flex flex-col justify-left gap-10 items-left">
+                    <div ref={oneRef} className="w-full flex flex-col justify-left gap-10 items-left">
                         <Image
                             src={invidi}
                             alt="insight firstly"
@@ -41,7 +54,7 @@ export default function Story() {
                         </div>
                     </div>
 
-                    <div className="w-full flex flex-col justify-left gap-10 items-left pt-40">
+                    <div ref={twoRef} className="w-full flex flex-col justify-left gap-10 items-left pt-40">
                         <Image
                             src={invida}
                             alt="insight secondary"
