f6caa5abf489726dc93c5e93124c3798d89531d4
Jazzyra <127509175+JihadZaidan@users.noreply.github.com>
2025-08-15 08:55:17 +0700

    feat(page): added and route to blog detail

--- src/app/blog-detail/page.tsx
+++ src/app/blog-detail/page.tsx
@@ -1,10 +1,12 @@
-// import DescribeDetail from "@/components/blog-detail/description";
+import DescribeDetail from "@/components/blog-detail/description";
 import DetailUpper from "@/components/blog-detail/detailHead";
 
 export default function BlogDetails() {
     return (
         <div className="w-full">
             <DetailUpper/>
+            <DescribeDetail/>
         </div>
     )
+
 }
