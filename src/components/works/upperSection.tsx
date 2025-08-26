10c3744243137046d635c47d5a9111e9bb0a7aea
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-08-26 19:14:12 +0700

    resolve scale of photos component

--- src/components/works/upperSection.tsx
+++ src/components/works/upperSection.tsx
@@ -14,39 +14,39 @@ export default function UpperWorks() {
     useSlideFromLeft(headerRef, 0.3);
     useSlideFromLeft(describeRef, 0.3);
     useSlideFromRight(imageRef, 0.3);
-    
     return (
         <section>
-            <div className="max-w-full w-full px-20 py-16 bg-white">
+            <div className="max-w-full w-full lg:px-20 lg:py-16 px-8 py-8 bg-white">
                 <div className="w-full flex flex-col gap-36 relative lg:top-10">
-                    <h1 ref={headerRef} className="text-5xl leading-[130%] w-[75%] font-normal font-sans text-black">
-                        From creative projects that push <br />
-                        the boundaries of our team&apos;s <br />
-                        imagination. Build in into all of <br />
+                    <h1 ref={headerRef} className="lg:text-5xl text-2xl leading-[130%] lg:w-[60%] font-normal font-sans text-black">
+                        From creative projects that push 
+                        the boundaries of our team&apos;s 
+                        imagination. Build in into all of 
                         our works.
                     </h1>
 
-                    <div className="w-full flex lg:flex-row flex-col lg:pl-[404px] lg:gap-24 gap-10">
-                        <div ref={describeRef} className="justify-left items-left flex flex-col gap-6 bg-transparent">
+                    <div className="w-full flex lg:flex-row flex-col lg:pl-[304px] lg:gap-24 gap-10">
+                        <div ref={describeRef} className="w-[55%] justify-left items-left flex flex-col gap-6 bg-transparent">
                             <h5 className="text-2xl font-medium text-left text-black">/ HOUSEL</h5>
-                            <h4 className="text-4xl font-medium text-left text-black">
-                                Help you to sell, buy, <br />
-                                mortgage, invest <br />
-                                property, and other <br />
-                                real estate service. <br />
+                            <h4 className="lg:text-3xl text-base font-medium text-left text-black">
+                                Help you to sell, buy, 
+                                mortgage, invest 
+                                property, and other 
+                                real estate service.
                             </h4>
                             <div>
-                                <Button className="justify-left gap-[9px] ml-[-20px]">
-                                    <p className="text-black text-xl font-medium text-left">View case study</p>
+                                <Button className="justify-left gap-[9px] ml-[-10px]">
+                                    <p className="text-black lg:text-xl text-base font-medium text-left">View case study</p>
                                     <ArrowRight height={23} width={23} className="mt-[2px] text-black" />
                                 </Button>
                             </div>
                         </div>
 
-                        <div ref={imageRef} className="lg:w-[40%] w-full z-7 relative lg:top-15">
+                        <div ref={imageRef} className="lg:w-full w-full z-7 relative lg:top-15">
                             <Image
                                 src={work1}
                                 alt="worker first photos"
+                                className="w-full"
                             />
                         </div>
                     </div>
