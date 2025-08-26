177dc8c5230cc06001f01c6f2066f309dde4bf01
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-08-26 19:39:53 +0700

    resolving position and text size

--- src/components/works/centerwork.tsx
+++ src/components/works/centerwork.tsx
@@ -19,9 +19,9 @@ export default function CenterWork() {
     useSlideFromRight(seimageRef, 0.3);
 
     return (
-        <div className="max-w-full w-full px-20 py-20 bg-black">
+        <div className="max-w-full w-full lg:px-20 lg:py-20 px-5 py-8 bg-black">
             <div className="w-full flex flex-col lg:gap-[-64px] gap-16">
-                <div className="w-full justify-left items-left flex lg:flex-row flex-col-reverse gap-18">
+                <div className="w-full justify-left items-left flex lg:flex-row flex-col-reverse gap-12">
                     <div ref={fimageRef} className="lg:w-[40%] w-full">
                         <Image
                             src={work2}
@@ -31,25 +31,25 @@ export default function CenterWork() {
 
                     <div ref={fibeRef} className="justify-left items-left flex flex-col gap-6 bg-transparent">
                         <h5 className="text-2xl font-medium text-left text-white">/ CONTEXT AI</h5>
-                        <h4 className="text-4xl font-medium text-left text-white">
+                        <h4 className="lg:text-4xl text-xl font-medium text-left text-white">
                             Empowers developers <br />
                             and companies to <br />
                             build outstanding <br />
                             products.<br />
                         </h4>
                         <div>
-                            <Button className="justify-left gap-[9px] ml-[-20px]">
-                                <p className="text-white text-xl font-medium text-left">View case study</p>
+                            <Button className="justify-left gap-[9px] ml-[-10px]">
+                                <p className="text-white lg:text-xl text-base font-medium text-left">View case study</p>
                                 <ArrowRight height={23} width={23} className="mt-[2px] text-white" />
                             </Button>
                         </div>
                     </div>
                 </div>
 
-                <div className="w-full justify-right items-right flex lg:flex-row flex-col gap-18 lg:pl-100">
+                <div className="w-full justify-right items-right flex lg:flex-row flex-col gap-12 lg:pl-90">
                     <div ref={secobeRef} className="justify-left items-left flex flex-col gap-6 bg-transparent relative lg:top-25">
                         <h5 className="text-2xl font-medium text-left text-white">/ CLOODY</h5>
-                        <h4 className="text-4xl font-medium text-left text-white">
+                        <h4 className="lg:text-4xl text-xl font-medium text-left text-white">
                             Allowing users to <br />
                             upload and sync files <br />
                             to cloud storage and <br />
@@ -58,8 +58,8 @@ export default function CenterWork() {
                             or specific devices. <br />
                         </h4>
                         <div>
-                            <Button className="justify-left gap-[9px] ml-[-20px]">
-                                <p className="text-white text-xl font-medium text-left">View case study</p>
+                            <Button className="justify-left gap-[9px] ml-[-10px]">
+                                <p className="text-white lg:text-xl text-base font-medium text-left">View case study</p>
                                 <ArrowRight height={23} width={23} className="mt-[2px] text-white" />
                             </Button>
                         </div>
