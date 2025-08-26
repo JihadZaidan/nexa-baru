5a1460f9ef38023ba7ff4cab59d6636e12f4b631
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-08-26 14:06:17 +0700

    repair feedback

--- src/components/landing-page/projects.tsx
+++ src/components/landing-page/projects.tsx
@@ -28,17 +28,17 @@ export default function Worksheet() {
     useSlideFromRight(tangoRef, 0.3);
 
     return (
-        <div className="max-w-full w-full px-20 lg:py-36 pt-25 pb-30 gap-28 bg-white justify-left items-left">
+        <div className="max-w-full w-full lg:px-20 lg:py-36 py-14 px-5 gap-28 bg-white justify-left items-left">
             <div className="w-full justify-left gap-28 flex flex-col">
                 <div className="w-full lg:justify-between items-left flex lg:flex-row flex-col justify-left gap-10">
-                    <h2 ref={headRef} className="lg:w-[48%] w-full text-left text-5xl font-medium font-sans text-black leading-[150%]">
-                        The work we do, <br />
+                    <h2 ref={headRef} className="lg:w-[40%] lg:text-5xl w-full text-left text-3xl font-medium font-sans text-black leading-[150%]">
+                        The work we do,
                         and the people we help
                     </h2>
 
                     <div ref={buttonRef} className="relative lg:mt-25">
-                        <Button className="flex flex-row justify-center gap-3 ml-[-20px]">
-                            <p className="text-black lg:text-lg text-2xl font-medium">All Case Study</p>
+                        <Button className="flex flex-row justify-center gap-3 ml-[-8px]">
+                            <p className="text-black text-lg font-medium">All Case Study</p>
                             <ArrowRight height={22} width={22} className="mt-[2px] text-black lg:h-[22px] lg:w-[22px] h-[25px] w-[25px]" />
                         </Button>
                     </div>
@@ -48,14 +48,14 @@ export default function Worksheet() {
                     <div className="w-full justify-left items-left flex lg:flex-row flex-col gap-18">
                         <div ref={firstRef} className="justify-left items-left flex flex-col gap-6 bg-transparent">
                             <h5 className="text-2xl font-medium text-left text-black">/ HOUSEL</h5>
-                            <h4 className="text-4xl font-medium text-left text-black">
+                            <h4 className="lg:text-4xl text-2xl font-medium text-left text-black">
                                 Help you to sell, buy, <br />
                                 mortgage, invest <br />
                                 property, and other <br />
                                 real estate service. <br />
                             </h4>
                             <div>
-                                <Button className="justify-left gap-[9px] ml-[-20px]">
+                                <Button className="justify-left gap-[9px] ml-[-10px]">
                                     <p className="text-black text-xl font-medium text-left">View case study</p>
                                     <ArrowRight height={23} width={23} className="mt-[2px] text-black" />
                                 </Button>
@@ -73,14 +73,14 @@ export default function Worksheet() {
                     <div className="w-full justify-right items-right lg:ml-105 flex lg:flex-row flex-col gap-18">
                         <div ref={secondRef} className="justify-left items-left flex flex-col gap-6 bg-transparent">
                             <h5 className="text-2xl font-medium text-left text-black">/ CONTEXT AI</h5>
-                            <h4 className="text-4xl font-medium text-left text-black">
+                            <h4 className="lg:text-4xl text-2xl font-medium text-left text-black">
                                 Empowers developers <br />
                                 and companies to <br />
                                 build outstanding <br />
-                                products.<br />
+                                products.
                             </h4>
                             <div>
-                                <Button className="justify-left gap-[9px] ml-[-20px]">
+                                <Button className="justify-left gap-[9px] ml-[-10px]">
                                     <p className="text-black text-xl font-medium text-left">View case study</p>
                                     <ArrowRight height={23} width={23} className="mt-[2px] text-black" />
                                 </Button>
@@ -98,16 +98,16 @@ export default function Worksheet() {
                     <div className="w-full justify-left items-left flex lg:ml-[-220px] lg:flex-row-reverse flex-col-reverse gap-18">
                         <div ref={thirdRef} className="justify-left items-left flex flex-col gap-6 bg-transparent">
                             <h5 className="text-2xl font-medium text-left text-black">/ CLOODY</h5>
-                            <h4 className="text-4xl font-medium text-left text-black">
+                            <h4 className="lg:text-4xl text-2xl font-medium text-left text-black">
                                 Allowing users to <br/>
                                 upload and sync files <br/>
                                 to cloud storage and <br/>
                                 then access <br/>
                                 them via Web Browser <br/>
-                                or specific devices. <br/>
+                                or specific devices. 
                             </h4>
                             <div>
-                                <Button className="justify-left gap-[9px] ml-[-20px]">
+                                <Button className="justify-left gap-[9px] ml-[-10px]">
                                     <p className="text-black text-xl font-medium text-left">View case study</p>
                                     <ArrowRight height={23} width={23} className="mt-[2px] text-black" />
                                 </Button>
