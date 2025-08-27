519611ea59d0a6ef08a80b047a6c1fcba68eba6c
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-08-27 10:42:01 +0700

    repair structure ui style

--- src/components/about/aboutHead.tsx
+++ src/components/about/aboutHead.tsx
@@ -16,19 +16,19 @@ export default function UpperAbout() {
     useFadeIn(imageRef, 0.3);
 
     return (
-        <div className="max-w-full w-full px-20 lg:pt-16 lg:pb-0 py-16 bg-white">
+        <div className="max-w-full w-full lg:px-20 lg:pt-16 lg:pb-0 px-8 pt-8 pb-8 bg-white">
             <div className="w-full flex flex-col lg:gap-0 gap-12">
                 <div className="w-full flex flex-col gap-12 lg:pl-150">
-                    <h1 ref={headRef} className="font-sans font-medium text-5xl text-black leading-[150%]">
+                    <h1 ref={headRef} className="font-sans font-medium lg:text-4xl text-lg text-black leading-[150%]">
                         An digital agency focused on digital <br />
                         experiences. With every single one <br />
                         of our clients, we bring forth a deep <br />
                         passion for creative problem solving.
                     </h1>
 
-                    <div ref={buttonRef} className="lg:ml-120">
+                    <div ref={buttonRef} className="lg:ml-80">
                         <Button className="justify-center gap-[9px] ml-[-10px] border-b-2 border-black">
-                            <p className="text-black text-2xl font-medium">Learn More About Us</p>
+                            <p className="text-black lg:text-2xl text-lg font-medium">Learn More About Us</p>
                             <ArrowRight height={22} width={22} className="mt-[2px] text-black" />
                         </Button>
                     </div>
