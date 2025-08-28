1d28963eb00480fa211977a1884d4280972634ca
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-08-28 09:35:41 +0700

    style(text): repair size

--- src/components/insight/insightHead.tsx
+++ src/components/insight/insightHead.tsx
@@ -10,14 +10,14 @@ export default function Insider() {
     useSlideFromRight(graphRef, 0.3);
 
     return (
-        <div className="max-w-full w-full bg-white lg:px-20 lg:pt-16 lg:pb-20 px-9 pt-9 pb-14">
+        <div className="max-w-full w-full bg-white lg:px-18 lg:pt-16 lg:pb-20 px-9 pt-5 pb-8">
             <div className="w-full justify-left items-left flex flex-col lg:gap-25 gap-16">
-                <h1 ref={headRef} className="w-full text-left text-5xl font-medium font-sans text-black leading-[150%]">
+                <h1 ref={headRef} className="w-full text-left lg:text-7xl text-3xl font-medium font-sans text-black leading-[150%]">
                     Digital Diaries: <br />
                     Strategies and Insights for Success
                 </h1>
 
-                <p ref={graphRef} className="lg:text-right text-black font-sans font-medium text-3xl leading-[140%] lg:ml-50">
+                <p ref={graphRef} className="lg:text-right text-black font-sans font-medium lg:text-3xl text-lg leading-[140%] lg:ml-50">
                     — Unlocking Strategies, Trends, <br/>
                     and Insights to Propel Your Brand <br/>
                     Forward in the Dynamic World of <br/>
@@ -27,4 +27,3 @@ export default function Insider() {
         </div>
     )
 }
-
